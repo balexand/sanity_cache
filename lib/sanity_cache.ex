e278e33be6830949c253f1082ef04c567ad67de6
Brian Alexander <balexand@gmail.com>
2022-03-12 13:26:25 -0700

    initial commit

---
+++ lib/sanity_cache.ex
@@ -0,0 +1,18 @@
+defmodule SanityCache do
+  @moduledoc """
+  Documentation for `SanityCache`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SanityCache.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sanity_cache_test.exs
@@ -0,0 +1,8 @@
+defmodule SanityCacheTest do
+  use ExUnit.Case
+  doctest SanityCache
+
+  test "greets the world" do
+    assert SanityCache.hello() == :world
+  end
+end
